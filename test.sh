3711bd7be2350e6e17fd308ac7b5cd4c39ace3cb
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-12-08 10:45:53 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 test_method='aes-128-ctr aes-128-cfb aes-128-gcm aes-256-ctr aes-256-cfb aes-256-gcm chacha20 chacha20-ietf chacha20-ietf-poly1305 rc4-md5 xchacha20-ietf-poly1305'
 
 gen_img_file(){
-	dd if=/dev/zero of=/tmp/test.img bs=1M count=300
+	dd if=/dev/zero of=/tmp/test.img bs=1M count=600
 	ln -sf /tmp/test.img /www/test.img
 }
 
