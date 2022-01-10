bac670d0037c3b13426c619ce6529d83a1f6b53f
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-01-10 15:12:00 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,42 +1,50 @@
 #!/bin/ash
 
-test_method='aes-128-ctr aes-128-cfb aes-128-gcm aes-256-ctr aes-256-cfb aes-256-gcm chacha20 chacha20-ietf chacha20-ietf-poly1305 rc4-md5 xchacha20-ietf-poly1305'
+test_method='aes-128-ctr aes-128-cfb aes-256-ctr aes-256-cfb chacha20 chacha20-ietf rc4-md5'
 
 gen_img_file(){
 	dd if=/dev/zero of=/tmp/test.img bs=1M count=600
 	ln -sf /tmp/test.img /www/test.img
 }
 
-gen_ss_json(){
-	cat <<-EOF > $PWD/ss.json
+gen_ssr_json(){
+	cat <<-EOF > $PWD/ssr.json
 	{
-	  "server":"127.0.0.1",
-	  "server_port":8388,
-	  "local_port":1080,
-	  "password":"password",
-	  "timeout":300,
-	  "method":"$1",
-	  "reuse_port":true
+	 "server": "127.0.0.1",
+	 "server_port": 8388,
+	 "local_port":1080,
+	 "password": "password",
+	 "method": "$1",
+	 "protocol": "origin",
+	 "protocol_param": "",
+	 "obfs": "plain",
+	 "obfs_param": "",
+	 "timeout": 120,
+	 "udp_timeout": 60,
+	 "fast_open": false,
+	 "workers": 4,
+	 "reuse_port":true
 	}
+
 	EOF
 }
 
-launch_ss(){
+launch_ssr(){
 	i=0
 	while [ $i -lt 4 ]
 	do
-		ss-server -c $PWD/ss.json >/dev/null 2>&1 &
-		ss-local -c  $PWD/ss.json >/dev/null 2>&1 &
+		ssr-server -c $PWD/ssr.json >/dev/null 2>&1 &
+		ssr-local -c  $PWD/ssr.json >/dev/null 2>&1 &
 		let i++
 	done
 }
 
 stop_ss(){
-	killall ss-server ss-local
+	killall ssr-server ssr-local
 }
 
 clean_all(){
-	rm -f /tmp/test.img /www/test.img $PWD/ss.json $PWD/*curl_info
+	rm -f /tmp/test.img /www/test.img $PWD/ssr.json $PWD/*curl_info
 }
 
 main(){
@@ -44,23 +52,22 @@ main(){
 	gen_img_file
 	for method in $test_method
 	do
-		gen_ss_json $method
-		launch_ss
+		gen_ssr_json $method
+		launch_ssr
 		curl --socks5 127.0.0.1 127.0.0.1/test.img -o /dev/null 2>&1 | tee $PWD/${method}_curl_info
 		stop_ss
 	done
 	set +x
 	echo
-	echo ----------------- friendlyarm -----------------
+	echo ----------------- tlnmsl friendlyarm -----------------
 	for method in $test_method
 	do
 		echo "          Method $method speed: `cat ${method}_curl_info |grep '100'|awk -F ' ' '{print $(NF-5)}'`"
 	done
-	echo ----------------- friendlyarm -----------------
+	echo ----------------- tlnmsl friendlyarm -----------------
 	echo
 	set -x
 	clean_all
 }
 
 main
-rm -rf test.sh
